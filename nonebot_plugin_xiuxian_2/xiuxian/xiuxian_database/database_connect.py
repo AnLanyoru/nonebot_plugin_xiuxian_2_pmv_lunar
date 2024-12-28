fbaec227c620ce0df1f274a8c8514d10a52c81a7
AnLanyoru <AnLanyoru@outlook.com>
2024-12-28 21:44:28 +0800

    增加数据库简单插入接口

--- nonebot_plugin_xiuxian_2/xiuxian/xiuxian_database/database_connect.py
+++ nonebot_plugin_xiuxian_2/xiuxian/xiuxian_database/database_connect.py
@@ -1,10 +1,9 @@
 import asyncpg
-
 from .database_config import database_config  # 这是上面的config()代码块，已经保存在config.py文件中
 from .. import DRIVER
 
 params = database_config()
-
+date_type_set = {str: "TEXT", int: "numeric", bytes: "bytea"}
 db_dict = {}
 
 
@@ -30,6 +29,54 @@ class DataBase:
             cursor = await db.fetch('SELECT version()')
             db_version = cursor[0]
             print(f"登录数据库成功，数据库版本：{db_version}")
+
+    async def update(self, table: str, where: dict, create_column: bool = 0, **kwargs):
+        """
+        简单逻辑数据更新接口
+        """
+        column_count = len(kwargs) + 1
+
+        update_column = ",".join([f"{column_name}=${count}" for column_name, count
+                                  in zip(kwargs.keys(), range(1, column_count))])
+
+        where_column = ",".join([f"{column_name}=${count}" for column_name, count
+                                 in zip(where.keys(), range(column_count, column_count + len(where)))])
+
+        async with self.pool.acquire() as db:
+            if create_column:
+                columns = list(*where.keys(), *kwargs.keys())
+                values = list(*where.values(), *kwargs.values())
+                for column, value in zip(columns, values):
+                    column_type = date_type_set.get(type(value))
+                    try:
+                        await db.execute(f"select {column} from {table}")
+                    except asyncpg.exceptions.UndefinedColumnError:
+                        sql = f"ALTER TABLE {table} ADD COLUMN {column} {column_type};"
+                await db.execute(sql)
+            sql = f"UPDATE {table} set {update_column} WHERE {where_column}"
+            await db.execute(sql, *kwargs.values(), *where.values())
+
+    async def insert(self, table: str, create_column: bool = 0, **kwargs):
+        """
+        简单逻辑数据更新接口
+        """
+        column_count = len(kwargs) + 1
+
+        insert_column = ",".join(kwargs)
+
+        value_format = ",".join([f"${count}" for count in range(1, column_count)])
+
+        async with self.pool.acquire() as db:
+            if create_column:
+                for column, value in kwargs.items():
+                    column_type = date_type_set.get(type(value))
+                    try:
+                        await db.execute(f"select {column} from {table}")
+                    except asyncpg.exceptions.UndefinedColumnError:
+                        sql = f"ALTER TABLE {table} ADD COLUMN {column} {column_type};"
+                await db.execute(sql)
+            sql = f"""INSERT INTO {table} ({insert_column}) VALUES ({value_format})"""
+            await db.execute(sql, *kwargs.values())
 
 
 database = DataBase()
