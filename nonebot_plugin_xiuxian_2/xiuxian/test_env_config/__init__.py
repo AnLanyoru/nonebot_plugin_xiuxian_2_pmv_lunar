9e52ff86083394277ababd8ab5e1089128448e7f
AnLanyoru <AnLanyoru@outlook.com>
2025-02-22 23:13:26 +0800

    修改进出测试模式指令

--- nonebot_plugin_xiuxian_2/xiuxian/test_env_config/__init__.py
+++ nonebot_plugin_xiuxian_2/xiuxian/test_env_config/__init__.py
@@ -3,9 +3,10 @@ from nonebot.adapters.onebot.v11 import Bot, GROUP, GroupMessageEvent
 
 from ..xiuxian_utils.lay_out import Cooldown, test_user
 
-test_mode_open = on_command("测试调试模式", priority=14, permission=GROUP, block=True)
+test_mode_open = on_command("开启测试模式", priority=14, permission=GROUP, block=True)
+test_mode_close = on_command("关闭测试模式", priority=14, permission=GROUP, block=True)
 
-test_list = [992551767, 35597641]
+test_list = [992551767, 35597641, 260706949, 685823074, 169698047, 977888070, 655738805, 441081340, 102221518]
 
 
 @test_mode_open.handle(parameterless=[Cooldown(cd_time=0, pass_test_check=True)])
@@ -17,8 +18,7 @@ async def test_mode_open_(bot: Bot, event: GroupMessageEvent):
         await bot.send(event=event, message=msg)
         await test_mode_open.finish()
     if user_id in test_user:
-        msg = '已退出测试模式'
-        test_user.remove(user_id)
+        msg = '已在测试模式中'
         await bot.send(event=event, message=msg)
         await test_mode_open.finish()
 
@@ -26,3 +26,22 @@ async def test_mode_open_(bot: Bot, event: GroupMessageEvent):
     msg = '已加入测试模式'
     await bot.send(event=event, message=msg)
     await test_mode_open.finish()
+
+
+@test_mode_close.handle(parameterless=[Cooldown(cd_time=0, pass_test_check=True)])
+async def test_mode_close_(bot: Bot, event: GroupMessageEvent):
+    """测试模式切换"""
+    user_id = str(event.user_id)
+    if int(user_id) not in test_list:
+        msg = '不在测试名单内'
+        await bot.send(event=event, message=msg)
+        await test_mode_close.finish()
+    if user_id in test_user:
+        msg = '已退出测试模式'
+        test_user.remove(user_id)
+        await bot.send(event=event, message=msg)
+        await test_mode_close.finish()
+
+    msg = '未处于测试模式'
+    await bot.send(event=event, message=msg)
+    await test_mode_close.finish()
